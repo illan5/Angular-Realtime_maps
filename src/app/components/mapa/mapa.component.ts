b17b68d546d3f33016770e8663f8f4ac49fa54be
Jose Illan <jose_illan@hotmail.com>
2020-05-26 21:24:59 +0200

    Configure maps library and show in html

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  mapa: mapboxgl.Map;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.crearMapa();
+  }
+
+  crearMapa() {
+    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiaWxsYW41IiwiYSI6ImNrYW4xYWN5dTE1b3gyeW82MXpzaDA5NW8ifQ.cr7S55BP2y7fjcFc1XuwLA';
+
+    this.mapa = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-75.75512993582937, 45.349977429009954],
+      zoom: 15.8
+    });
   }
 
 }
