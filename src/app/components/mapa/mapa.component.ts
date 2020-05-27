30736b13275693d45de10f337bf888c05ea0bc55
Jose Illan <jose_illan@hotmail.com>
2020-05-27 19:57:47 +0200

    Implement remove marker

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -55,14 +55,23 @@ export class MapaComponent implements OnInit {
 
   agregarMarcador( marcador: Lugar ) {
 
-    const html = `<h2>${ marcador.nombre }</h2>
-                  <br>
-                  <button>Borrar</button>`;
+    // const html = `<h2>${ marcador.nombre }</h2>
+    //               <br>
+    //               <button>Borrar</button>`;
+
+    const h2 = document.createElement('h2');
+    h2.innerText = marcador.nombre;
+
+    const btnBorrar = document.createElement('button');
+    btnBorrar.innerText = 'Borrar'
     
+    const div = document.createElement('div');
+    div.append(h2, btnBorrar);
+
     const customPopup = new mapboxgl.Popup({
       offset: 25,
       closeOnClick: false
-    }).setHTML( html );
+    }).setDOMContent( div );
 
     const marker = new mapboxgl.Marker({
       draggable: true,
@@ -78,7 +87,14 @@ export class MapaComponent implements OnInit {
 
       //TODO: Crear evento al mover el marcador
 
+    });
+
+    btnBorrar.addEventListener( 'click', () => {
+      marker.remove();
+
+      //TODO: Eliminar mediante socket
     })
+
   }
 
   crearMarcador() {
